6791c1812f6bacbe83a03729004ea1e32daa447b
Ignacio Barahona <ignacio.barahona@usach.cl>
2022-01-06 05:33:13 -0300

    Fix consulta secciones por jornada
    Se arregla consulta para rol coordinador

--- app/controllers/secciones_controller.rb
+++ app/controllers/secciones_controller.rb
@@ -59,7 +59,7 @@ class SeccionesController < ApplicationController
     semestre_actual = Semestre.where('activo = ? AND borrado = ?', true, false).last
     usuario = Usuario.find(current_usuario.id)
     if usuario.rol.rango == 1
-      secciones = Seccion.where('semestre_id = ? AND borrado = ?', semestre_actual.id, false)
+      secciones = Seccion.where('semestre_id = ? AND jornada_id = ? AND borrado = ?', semestre_actual.id, params[:idJornada], false)
     elsif usuario.rol.rango == 2
       secciones = Seccion.joins(:profesores).where('profesores.usuario_id = ? AND semestre_id = ? AND jornada_id = ? AND borrado = ?', usuario.id, semestre_actual.id, params[:idJornada], false)
     end
